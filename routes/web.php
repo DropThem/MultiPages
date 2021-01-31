b688ccab020699be58363cf361277ee7400ab63c
Dropthem <dropthem@protonmail.com>
2021-01-31 22:57:44 +0100

    Include Tout les pages

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view("welcome");
+});
+Route::get('/about', function () {
+    return view("pages.about");
+});
+Route::get('/store', function () {
+    return view("pages.store");
+});
+Route::get('/products', function () {
+    return view("pages.products");
 });
